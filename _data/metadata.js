269cc9773d28db20e96c0c64f6fe37cf730de9b4
Josh Wayne <josh@joshwayne.com>
2023-05-20 11:43:06 -0600

    copy tweaks

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,8 +1,9 @@
 module.exports = {
 	title: "Break into freelance",
-	url: "https://breakintofreelance.com/",
+	url: "https://breakintofreelance.design/",
 	language: "en",
-	description: "Break into freelance is a beginner’s guide to freelancing for UX/UI designers.",
+	description:
+		"Break into freelance is a beginner’s guide to freelancing for UX/UI designers.",
 	author: {
 		name: "Josh Wayne",
 		email: "josh@joshwayne.com",
